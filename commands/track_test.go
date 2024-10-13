06f0258e040e5497793e79455b0a887aac790754
AnnatarHe <annatar.he+github@gmail.com>
2024-10-13 12:55:48 +0800

    tests(track): add cursor logic in track command

--- commands/track_test.go
+++ commands/track_test.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"sync"
 	"testing"
 	"time"
@@ -94,6 +95,9 @@ func (s *trackTestSuite) TestMultipTrackWithPre() {
 }
 
 func (s *trackTestSuite) TestTrackWithSendData() {
+
+	syncTimes := 0
+
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authorizationHeader := r.Header.Get("Authorization")
 		body, err := io.ReadAll(r.Body)
@@ -109,6 +113,7 @@ func (s *trackTestSuite) TestTrackWithSendData() {
 
 		assert.Contains(s.T(), string(body), "fish")
 		assert.EqualValues(s.T(), "CLI TOKEN001", authorizationHeader)
+		syncTimes++
 		w.WriteHeader(http.StatusNoContent)
 	}))
 	defer server.Close()
@@ -138,7 +143,7 @@ func (s *trackTestSuite) TestTrackWithSendData() {
 		},
 	}
 
-	times := 10
+	times := 16
 
 	var wg sync.WaitGroup
 	wg.Add(times)
@@ -174,6 +179,8 @@ func (s *trackTestSuite) TestTrackWithSendData() {
 
 	wg.Wait()
 
+	assert.EqualValues(s.T(), 2, syncTimes)
+
 	// Check the number of lines in the COMMAND_PRE_STORAGE_FILE
 	preFile := os.ExpandEnv("$HOME/" + model.COMMAND_PRE_STORAGE_FILE)
 	content, err := os.ReadFile(preFile)
@@ -186,6 +193,43 @@ func (s *trackTestSuite) TestTrackWithSendData() {
 		}
 	}
 	assert.Equal(s.T(), times, lines)
+
+	// Check the number of lines in the COMMAND_POST_STORAGE_FILE
+	postFile := os.ExpandEnv("$HOME/" + model.COMMAND_POST_STORAGE_FILE)
+	postContent, err := os.ReadFile(postFile)
+	assert.Nil(s.T(), err)
+
+	postLines := 0
+	for _, byte := range postContent {
+		if byte == '\n' {
+			postLines++
+		}
+	}
+	assert.Equal(s.T(), lines, postLines)
+
+	// Check the CURSOR_FILE
+	cursorFile := os.ExpandEnv("$HOME/" + model.COMMAND_CURSOR_STORAGE_FILE)
+	cursorContent, err := os.ReadFile(cursorFile)
+	assert.Nil(s.T(), err)
+
+	cursorLines := 0
+	var cursorValues []time.Time
+	for _, line := range strings.Split(string(cursorContent), "\n") {
+		if line != "" {
+			cursorLines++
+			nanoTime, err := strconv.ParseInt(line, 10, 64)
+			assert.Nil(s.T(), err)
+			cursorValues = append(cursorValues, time.Unix(0, nanoTime))
+		}
+	}
+	assert.Equal(s.T(), 2, cursorLines)
+	assert.Len(s.T(), cursorValues, 2)
+	assert.True(s.T(), cursorValues[1].After(cursorValues[0]))
+
+	// Check if cursor values exist in postContent
+	for _, value := range cursorValues {
+		assert.Contains(s.T(), string(postContent), strconv.FormatInt(value.UnixNano(), 10))
+	}
 }
 
 func (s *trackTestSuite) TearDownSuite() {
