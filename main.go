10775191428a76ac2d2c7ac69675dc724e980c15
AnnatarHe <annatar.he+github@gmail.com>
2024-11-16 13:32:52 +0800

    fix(cli): add version field

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/malamtime/cli/commands"
@@ -12,27 +13,55 @@ import (
 var GitCommit string
 
 func main() {
+	cli.VersionFlag = &cli.BoolFlag{
+		Name:    "version",
+		Aliases: []string{"v"},
+		Usage:   "print the version",
+	}
+
+	cli.VersionPrinter = func(cCtx *cli.Context) {
+		fmt.Fprintf(cCtx.App.Writer, "version=%s\n", cCtx.App.Version)
+	}
+
 	configFile := os.ExpandEnv("$HOME/.malamtime/config.toml")
 	configService := model.NewConfigService(configFile)
 
 	model.InjectVar(GitCommit)
 	commands.InjectVar(GitCommit, configService)
-	app := &cli.App{
-		Name:        "MalamTime CLI",
-		Description: "MalamTime CLI for track DevOps works",
-		Usage:       "MalamTime CLI for track DevOps works",
-		Version:     GitCommit,
-		Commands: []*cli.Command{
-			commands.AuthCommand,
-			commands.TrackCommand,
-			commands.GCCommand,
+	app := cli.NewApp()
+	app.Name = "MalamTime CLI"
+	app.Description = "MalamTime CLI for track DevOps works"
+	app.Usage = "MalamTime CLI for track DevOps works"
+	app.Version = GitCommit
+	app.Copyright = "Copyright (c) 2024 shelltime.xyz Team"
+	app.Authors = []*cli.Author{
+		{
+			Name:  "shelltime.xyz Team",
+			Email: "annatar.he+shelltime.xyz@gmail.com",
 		},
 	}
+	app.Suggest = true
+	app.HideVersion = false
+	app.Metadata = map[string]interface{}{
+		"version": GitCommit,
+	}
 
+	app.Commands = []*cli.Command{
+		commands.AuthCommand,
+		commands.TrackCommand,
+		commands.GCCommand,
+		{
+			Name:    "version",
+			Aliases: []string{"v"},
+			Action: func(ctx *cli.Context) error {
+				fmt.Println(GitCommit)
+				return nil
+			},
+		},
+	}
 	err := app.Run(os.Args)
 	if err != nil {
 		logrus.Errorln(err)
 	}
-	// every commands init logger, and here to close
 	commands.CloseLogger()
 }
