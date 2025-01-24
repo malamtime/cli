183a226db2fbfa5ee6dc46bafa4acaebdb493b4d
AnnatarHe <annatar.he+github@gmail.com>
2025-01-25 01:21:49 +0800

    fix(daemon): nack if message not handled

--- daemon/handlers.go
+++ daemon/handlers.go
@@ -16,16 +16,17 @@ func SocketTopicProccessor(messages <-chan *message.Message) {
 		var socketMsg SocketMessage
 		if err := msgpack.Unmarshal(msg.Payload, &socketMsg); err != nil {
 			slog.ErrorContext(ctx, "failed to parse socket message", slog.Any("err", err))
+			msg.Nack()
 		}
 
 		if socketMsg.Type == SocketMessageTypeSync {
-			if err := handlePubSubSync(ctx, socketMsg.Payload); err != nil {
+			err := handlePubSubSync(ctx, socketMsg.Payload)
+			if err != nil {
 				slog.ErrorContext(ctx, "failed to parse socket message", slog.Any("err", err))
+				msg.Nack()
+			} else {
+				msg.Ack()
 			}
 		}
-
-		// we need to Acknowledge that we received and processed the message,
-		// otherwise, it will be resent over and over again.
-		msg.Ack()
 	}
 }
